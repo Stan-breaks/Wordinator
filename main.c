3c3723224d5308bf30ab56ad7a9cab42cab12936
Stan-breaks <stanleymwendwa03@gmail.com>
2025-06-10 13:42:31 +0300

    I am bad at C

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 // Copyright 2025 <Stanbreaks>
 #include <stdio.h>
+#include <stdlib.h>
 int main() {
-  char input[20];
-  scanf("%s", input);
-  printf("%s\n", input);
+  char input[100];
+  if (scanf("%s", input) != 1) {
+    printf("Failed to get input");
+    return 0;
+  }
+  char *endptr;
+  long num = strtol(input, &endptr, 10);
+  printf("%ld\n", num);
+  printf("%d", endptr);
 }
